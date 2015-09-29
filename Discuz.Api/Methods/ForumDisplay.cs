c565ac18cf96942729e4ce90765a092d892d6290
gruan01 <gruan@asnum.com>
2015-09-29 11:07:11 +0800

    dll

---
+++ Discuz.Api/Methods/ForumDisplay.cs
@@ -0,0 +1,68 @@
+﻿using Discuz.Api.Attributes;
+using Discuz.Api.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discuz.Api.Methods {
+    public class ForumDisplay : MethodBase<IEnumerable<ThreadSummary>> {
+        public override string Module {
+            get {
+                return "forumdisplay";
+            }
+        }
+
+        /// <summary>
+        /// fid
+        /// </summary>
+        [Param("fid", Required = true)]
+        public int ID {
+            get;
+            set;
+        }
+
+        private int page = 1;
+        [Param("page")]
+        public int Page {
+            get {
+                return this.page;
+            }
+            set {
+                this.page = value < 1 ? 1 : value;
+            }
+        }
+
+        private int pageSize = 20;
+        [Param("tpp")]
+        public int PageSize {
+            get {
+                return this.pageSize;
+            }
+            set {
+                this.pageSize = value < 1 ? 1 : value;
+            }
+        }
+
+
+        internal override async Task<IEnumerable<ThreadSummary>> Execute(ApiClient client) {
+            var result = await this.GetResult(client);
+
+            var o = new {
+                Variables = new {
+                    forum_threadlist = Enumerable.Empty<ThreadSummary>(),
+                    tpp = 0,
+                    page = (int?)0
+                }
+            };
+
+            o = JsonConvert.DeserializeAnonymousType(result, o);
+            this.Page = o.Variables.page ?? 1;
+            this.PageSize = o.Variables.tpp;
+
+            return o.Variables.forum_threadlist;
+        }
+    }
+}
