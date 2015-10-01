7ac7851ab0c1e17d6256c4a080e93221efcdf296
gruan01 <gruan@asnum.com>
2015-10-01 22:29:58 +0800

    Save Favorites and load favorite when launched.

--- Discuz/Discuz/ViewModels/TabViewModel.cs
+++ Discuz/Discuz/ViewModels/TabViewModel.cs
@@ -17,8 +17,11 @@ namespace Discuz.ViewModels {
             set;
         }
 
+        private SortedDictionary<int, string> Favorites = null;
+
         public TabViewModel(SimpleContainer container) {
             this.Container = container;
+            this.Favorites = PropertiesHelper.GetObject<SortedDictionary<int, string>>("Favorites") ?? new SortedDictionary<int, string>();
 
             this.Datas = new BindableCollection<Screen>();
             var index = container.GetInstance<ForumIndexViewModel>();
@@ -27,20 +30,35 @@ namespace Discuz.ViewModels {
             this.Datas.Add(index);
             this.Datas.Add(setting);
 
+            foreach (var kv in this.Favorites) {
+                this.ShowFavorite(kv.Key, kv.Value);
+            }
+
             this.RegistMessageCenter();
         }
 
         private void RegistMessageCenter() {
             MessagingCenter.Subscribe<ForumDetailViewModel, Tuple<int, string>>(this, ForumDetailViewModel.AddFavoriteMessage, (sender, arg) => {
-                this.AddToFavorite(arg.Item1, arg.Item2);
+                if (!this.Favorites.ContainsKey(arg.Item1)) {
+                    this.ShowFavorite(arg.Item1, arg.Item2);
+                    this.AddToFavorite(arg.Item1, arg.Item2);
+                }
             });
         }
 
-        private void AddToFavorite(int forumID, string name) {
+        private void ShowFavorite(int forumID, string name) {
             var vm = this.Container.GetInstance<ForumDisplayViewModel>("ForFavorite");
             vm.ID = forumID;
             vm.DisplayName = name;
-            this.Datas.Insert(0, vm);
+            this.Datas.Add(vm);
+        }
+
+        private async void AddToFavorite(int forumID, string name) {
+            if (!this.Favorites.ContainsKey(forumID)) {
+                this.Favorites.Add(forumID, name);
+                PropertiesHelper.SetObject("Favorites", this.Favorites);
+                await PropertiesHelper.Save();
+            }
         }
     }
 }
